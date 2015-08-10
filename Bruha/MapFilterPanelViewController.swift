8e7a46e0b2271a773ad0fdc773fefb115a14fb6b
Yafan Liu <liu352@mcmaster.ca>
2015-08-10 15:38:49 -0400

    map filter

---
+++ Bruha/MapFilterPanelViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapFilterPanelViewController.swift
+//  Bruha
+//
+//  Created by lye on 15/8/10.
+//  Copyright (c) 2015年 Bruha. All rights reserved.
+//
+
+import UIKit
+
+class MapFilterPanelViewController: UIViewController {
+
+    var panelControllerContainer: ARSPContainerController!
+    var swipeZoneHeight: CGFloat = 74
+    var visibleZoneHeight: CGFloat = 74
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.panelControllerContainer = self.parentViewController as! ARSPContainerController
+        panelControllerContainer.visibleZoneHeight = visibleZoneHeight
+        panelControllerContainer.shouldOverlapMainViewController = true
+        //println(panelControllerContainer.visibilityState)
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.swipeZoneHeight = self.panelControllerContainer.swipableZoneHeight
+        self.visibleZoneHeight = self.panelControllerContainer.visibleZoneHeight
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
--- Bruha/MapViewController.swift
+++ Bruha/MapViewController.swift
@@ -8,28 +8,28 @@
 
 import UIKit
 
-class MapViewController: UIViewController,CLLocationManagerDelegate, GMSMapViewDelegate {
+class MapViewController: UIViewController,CLLocationManagerDelegate, GMSMapViewDelegate,ARSPDragDelegate, ARSPVisibilityStateDelegate {
 
     @IBOutlet weak var viewMap: GMSMapView!
+    @IBOutlet weak var myLocation: UIButton!
     
-    @IBOutlet weak var bbFindAddress: UIBarButtonItem!
-    
-    @IBOutlet weak var lblInfo: UILabel!
-    
+    var panelControllerContainer: ARSPContainerController!
     var locationManager = CLLocationManager()
-
+    var didFindMyLocation = false
+    var locationMarker: GMSMarker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
-        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(48.857165, longitude: 2.354613, zoom: 8.0)
-        viewMap.camera = camera
         
-        viewMap.delegate = self
         viewMap.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.New, context: nil)
-
+        
+        self.panelControllerContainer = self.parentViewController as! ARSPContainerController
+        self.panelControllerContainer.dragDelegate = self
+        self.panelControllerContainer.visibilityStateDelegate = self
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -38,26 +38,69 @@ class MapViewController: UIViewController,CLLocationManagerDelegate, GMSMapViewD
     }
     
     
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if !didFindMyLocation {
+            let myLocation: CLLocation = change[NSKeyValueChangeNewKey] as! CLLocation
+            viewMap.camera = GMSCameraPosition.cameraWithTarget(myLocation.coordinate, zoom: 10.0)
+            //viewMap.settings.myLocationButton = true
+            
+            didFindMyLocation = true
+        }
+    }
     // MARK: IBAction method implementation
     
-    @IBAction func changeMapType(sender: AnyObject) {
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            viewMap.myLocationEnabled = true
+            //viewMap.settings.myLocationButton = true
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    // 5
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location = locations.first as? CLLocation {
+            
+            // 6
+            viewMap.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            
+            // 7
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func showAlertWithMessage(message: String) {
+        let alertController = UIAlertController(title: "GMapsDemo", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        alertController.addAction(closeAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    func panelControllerChangedVisibilityState(state:ARSPVisibilityState) {
+        //TODO
+        if(panelControllerContainer.shouldOverlapMainViewController){
+            if (state.value == ARSPVisibilityStateMaximized.value) {
+                self.panelControllerContainer.panelViewController.view.alpha = 1
+            }else{
+                self.panelControllerContainer.panelViewController.view.alpha = 1
+            }
+        }else{
+            self.panelControllerContainer.panelViewController.view.alpha = 1
+        }
+    }
+    
+    func panelControllerWasDragged(panelControllerVisibility : CGFloat) {
         
     }
     
-    
-    @IBAction func findAddress(sender: AnyObject) {
-        
+    @IBAction func myLocation(sender: AnyObject) {
+        //myLocation = viewMap.settings.myLocationButton
     }
-    
-    
-    @IBAction func createRoute(sender: AnyObject) {
-        
-    }
-    
-    
-    @IBAction func changeTravelMode(sender: AnyObject) {
-        
-    }
+
     
 
 }
