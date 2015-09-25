f73c88bffc4efc9d5b01b25053711b1493cbdb24
Graeme <graeme@showdom.com>
2015-09-25 12:49:06 -0400

    Added gradient

---
+++ Bruha/CAGradientLayer.swift
@@ -0,0 +1,27 @@
+//
+//  CAGradientLayer.swift
+//  Bruha
+//
+//  Created by Ryan O'Neill on 2015-09-25.
+//  Copyright (c) 2015 Bruha. All rights reserved.
+//
+
+import UIKit
+
+extension CAGradientLayer {
+    
+    func gradientColor() -> CAGradientLayer {
+        let bottomColor = UIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1)
+        let topColor = UIColor(red: 0/255.0, green: 92/255.0, blue: 92/255.0, alpha: 1)
+        
+        let gradientColor: [CGColor] = [topColor.CGColor, bottomColor.CGColor]
+        let gradientLocations: [Float] = [0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColor
+        gradientLayer.locations = gradientLocations
+        
+        return gradientLayer
+    }
+    
+}
--- Bruha/DashBoardViewController.swift
+++ Bruha/DashBoardViewController.swift
@@ -77,12 +77,20 @@ class DashBoardViewController: UIViewController {
         }
         performImageSegue()
         
+        backgroundGradient()
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func backgroundGradient() {
+        let background = CAGradientLayer().gradientColor()
+        background.frame = self.view.bounds
+        self.view.layer.insertSublayer(background, atIndex: 0)
     }
     
 
