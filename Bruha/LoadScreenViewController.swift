facae7e2bdafb5756593805cb1dcd460196546ae
Zhuoheng <wuzhsteven@gmail.com>
2015-11-03 10:34:54 -0500

    launch screen delay until download finish

--- Bruha/LoadScreenViewController.swift
+++ Bruha/LoadScreenViewController.swift
@@ -12,9 +12,35 @@ class LoadScreenViewController: UIViewController {
     
     let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     
+    var generalRetrieved = 0
+    var loggedinRetrieved = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        DeleteData(context: self.managedObjectContext).deleteAll()
+        LoadScreenService(context: self.managedObjectContext).retrieveAll()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueSplash", name:"Event Categories", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueSplash", name:"Events", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueDashBoard", name:"User Events", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueDashBoard", name:"Addicted Events", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueSplash", name:"Venues", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueDashBoard", name:"User Venues", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueDashBoard", name:"Addicted Venues", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueSplash", name:"Organizations", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueDashBoard", name:"User Organizations", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueDashBoard", name:"Addicted Organizations", object: nil)
         
     }
     
@@ -22,14 +48,12 @@ class LoadScreenViewController: UIViewController {
         
         if hasInternet() == "true" {
             
-            DeleteData(context: self.managedObjectContext).deleteAll()
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "performSegue", name:"download complete", object: nil)
             
-            LoadScreenService(context: self.managedObjectContext).retrieveAll()
-            
-            delay(3.0) {
-                print("GO TO DASHBOARD")
-                self.performSegue()
-            }
+//            delay(3.0) {
+//                print("GO TO DASHBOARD")
+//                self.performSegue()
+//            }
             
             
         } else {
@@ -58,12 +82,10 @@ class LoadScreenViewController: UIViewController {
         
     }
     
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     func hasInternet() -> String {
         
@@ -81,22 +103,50 @@ class LoadScreenViewController: UIViewController {
         return error
     }
     
+    func segueSplash() {
+        
+        generalRetrieved = generalRetrieved + 1
+        
+        if generalRetrieved == 4 && FetchData(context: managedObjectContext).fetchUserInfo()?.count == 0 {
+            print("should go to splash")
+            NSNotificationCenter.defaultCenter().postNotificationName("download complete", object: nil)
+            
+            /* not sure if I have to remove the observers? */
+            //NSNotificationCenter.defaultCenter().removeObserver(self, name:"Event Categories", object: nil)
+            //NSNotificationCenter.defaultCenter().removeObserver(self, name:"Events", object: nil)
+            
+        } else if generalRetrieved == 4 && loggedinRetrieved == 6 {
+            print("should dashboard")
+            NSNotificationCenter.defaultCenter().postNotificationName("download complete", object: nil)
+        }
+    }
+    
+    func segueDashBoard() {
+        
+        loggedinRetrieved = loggedinRetrieved + 1
+        
+        if loggedinRetrieved == 6 && FetchData(context: managedObjectContext).fetchUserInfo()?.count != 0 && generalRetrieved == 4 {
+            print("should go to dashboard")
+            NSNotificationCenter.defaultCenter().postNotificationName("download complete", object: nil)
+        }
+    }
     
     func performSegue() {
         
         if(FetchData(context: managedObjectContext).fetchUserInfo()?.count != 0) {
             
+            print("DashBoard")
             self.performSegueWithIdentifier("toDashBoard", sender: self)
             
         } else {
             
+            print("SplashView")
             self.performSegueWithIdentifier("toSplashView", sender: self)
-            
         }
     }
     
-    func delay(delay:Double, closure:()->()) {
-        dispatch_after( dispatch_time( DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)) ), dispatch_get_main_queue(), closure )
-    }
+//    func delay(delay:Double, closure:()->()) {
+//        dispatch_after( dispatch_time( DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)) ), dispatch_get_main_queue(), closure )
+//    }
     
 }
