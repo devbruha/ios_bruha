da557bdc9974fdb04cbfecaab055f5410af79aec
Hejie Yan <hejieyan@hotmail.com>
2015-12-31 23:31:38 -0500

    + Login Alert on Get Addicted in MapViewController and ExploreListViewController

--- Bruha/MapViewController.swift
+++ Bruha/MapViewController.swift
@@ -844,11 +844,7 @@ class MapViewController: UIViewController,UITableViewDataSource,UITableViewDeleg
                 
             } else {
                 
-                let alert = UIAlertView(title: "Please log in for this!!!", message: nil, delegate: nil, cancelButtonTitle: nil)
-                alert.show()
-                let delay = 5.0 * Double(NSEC_PER_SEC)
-                var time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-                alert.dismissWithClickedButtonIndex(-1, animated: true)
+                alertLogin()
                 
             }
             break
@@ -856,6 +852,23 @@ class MapViewController: UIViewController,UITableViewDataSource,UITableViewDeleg
             break
         }
 
+    }
+    
+    func alertLogin() {
+        let alertController = UIAlertController(title: "You are not logged in!", message:nil, preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
+        let loginAction = UIAlertAction(title: "Login", style: .Default) { (_) -> Void in
+            self.performSegueWithIdentifier("GoToLogin", sender: self) // Replace SomeSegue with your segue identifier (name)
+        }
+        let signupAction = UIAlertAction(title: "Signup", style: .Default) { (_) -> Void in
+            self.performSegueWithIdentifier("GoToSignup", sender: self) // Replace SomeSegue with your segue identifier (name)
+        }
+        alertController.addAction(signupAction)
+        alertController.addAction(loginAction)
+        alertController.addAction(cancelAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
     }
     
     func swipeableTableViewCell( cell : SWTableViewCell!,didTriggerRightUtilityButtonWithIndex index:NSInteger){
